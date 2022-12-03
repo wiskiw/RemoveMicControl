7a53b1bd39b08881c2231582baad35b519b6935f
Andrei Yablonski <ayablonski23@gmail.com>
2022-12-03 23:04:14 +0100

    Fix project structure

--- macos-app/BluetoothMicControl/BluetoothMicControl/BluetoothMicControlApp.swift
+++ macos-app/BluetoothMicControl/BluetoothMicControl/BluetoothMicControlApp.swift
@@ -11,31 +11,56 @@ import AVKit
 
 @main
 struct BluetoothMicControlApp: App {
-
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-            WindowGroup {
-                MicControlView(vm:MicControlViewModel())
-            }
+        WindowGroup {
+            MicControlView(vm:MicControlViewModel())
         }
-
-    init() {
-//        switch AVCaptureDevice.authorizationStatus(for: .audio) {
-//            case .notDetermined:
-//                AVCaptureDevice.requestAccess(for: .audio) { granted in
-//                    if !granted {
-//                        NSLog("Can't get access to the mic.")
-//                        exit(1)
-//                    }
-//                }
-//
-//            case .denied:
-//                fallthrough
-//            case .restricted:
-//                NSLog("Can't get access to the mic.")
-//                exit(1)
-//            default:
-//                print("Already has permission");
-//        }
     }
+    
+    init() {
+        //        switch AVCaptureDevice.authorizationStatus(for: .audio) {
+        //            case .notDetermined:
+        //                AVCaptureDevice.requestAccess(for: .audio) { granted in
+        //                    if !granted {
+        //                        NSLog("Can't get access to the mic.")
+        //                        exit(1)
+        //                    }
+        //                }
+        //
+        //            case .denied:
+        //                fallthrough
+        //            case .restricted:
+        //                NSLog("Can't get access to the mic.")
+        //                exit(1)
+        //            default:
+        //                print("Already has permission");
+        //        }
+    }
+}
+
+
+class AppDelegate : NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak fileprivate var menuBar: NSMenu!
+    
+    private var statusItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem =  NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+//        statusItem.menu = statusMenu
+        //        self.menuItemToggle.title = "Disable"
+        statusItem.button?.image = NSImage(systemSymbolName: "hammer", accessibilityDescription: nil)
+        
+        
+    }
+    
+    //    @IBAction func toggleAction(_ sender: NSMenuItem) {
+    //        self.hotkey!.toggle()
+    //    }
+    
     
 }
