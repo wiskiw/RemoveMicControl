cb70da5e15b7ca71b20ec1d40a382a87722f5a88
Andrei Yablonski <ayablonski23@gmail.com>
2022-12-05 01:45:17 +0100

    Move throttle operation to  OutputDeviceService

--- macos-app/BluetoothMicControl/BluetoothMicControl/Models/OutputDevice.swift
+++ macos-app/BluetoothMicControl/BluetoothMicControl/Models/OutputDevice.swift
@@ -10,62 +10,41 @@ import SimplyCoreAudio
 
 class OutputDevice : Equatable {
     
-    private let throttleQueue = OperationQueue()
     let audioDevice: AudioDevice
     
     private var lastVolume : Float32 = 0.0
-    private var volumeChangedObserver : NSObjectProtocol? = nil
     private var ignoreNext : Bool = false
     
     var volumeChangedListener: ((Float32, Float32)->(Bool))? = nil
     
     init (audioDevice: AudioDevice){
         self.audioDevice = audioDevice
-        self.throttleQueue.maxConcurrentOperationCount = 1
-        
         self.lastVolume = self.getVolume()
 
         //        NSLog("Init OutputDevice '\(audioDevice.name)', volume:\(self.lastVolume)")
-        
-        self.volumeChangedObserver = NotificationCenter.default.addObserver(forName: .deviceVolumeDidChange,
-                                                                            object: nil,
-                                                                            queue: .main) { (notification) in
-            self.handleVolumeChangedEvent()
-        }
     }
     
-    deinit {
-        if let observer = volumeChangedObserver {
-            NotificationCenter.default.removeObserver(observer)
-        }
-    }
-    
-    private func handleVolumeChangedEvent() {
-        let throttleOperation = VolumeChangedThrottleOperation() {
-            if (self.ignoreNext){
-                self.ignoreNext = false
-                return
-            }
-            
-            let old = self.lastVolume
-            let new = self.getVolume()
-            
-            if (old == new || self.volumeChangedListener == nil) {
-                return
-            }
-            
-            let isRollbackRequired = self.volumeChangedListener?(old, new) ?? false
-            
-            if (isRollbackRequired) {
-                self.ignoreNext = true
-                self.setVolume(volume: old)
-            } else {
-                self.lastVolume = new
-            }
+    func syncVolumeWithSystem() {
+        if (self.ignoreNext){
+            self.ignoreNext = false
+            return
         }
         
-        self.throttleQueue.cancelAllOperations()
-        self.throttleQueue.addOperation(throttleOperation)
+        let old = self.lastVolume
+        let new = self.getVolume()
+        
+        if (old == new || self.volumeChangedListener == nil) {
+            return
+        }
+        
+        let isRollbackRequired = self.volumeChangedListener?(old, new) ?? false
+        
+        if (isRollbackRequired) {
+            self.ignoreNext = true
+            self.setVolume(volume: old)
+        } else {
+            self.lastVolume = new
+        }
     }
     
     func getVolume() -> Float32 {
@@ -78,30 +57,5 @@ class OutputDevice : Equatable {
     
     static func == (lhs: OutputDevice, rhs: OutputDevice) -> Bool {
         return lhs.audioDevice.id == rhs.audioDevice.id
-    }
-    
-    
-    class VolumeChangedThrottleOperation: Operation {
-        
-        private static let throttleDelayMs = 96
-        private let onComplete : (() -> Void)!
-        
-        init(onComplete : @escaping () -> Void) {
-            self.onComplete = onComplete
-        }
-        
-        override func main() {
-//            NSLog("Trottling...")
-            sleepMs(durationMs: VolumeChangedThrottleOperation.throttleDelayMs)
-            
-            if (isCancelled){
-                return
-            } else {
-                DispatchQueue.main.async(){
-//                    NSLog("Notifing...")
-                    self.onComplete()
-                }
-            }
-        }
     }
 }
--- macos-app/BluetoothMicControl/BluetoothMicControl/Services/OutputDeviceService.swift
+++ macos-app/BluetoothMicControl/BluetoothMicControl/Services/OutputDeviceService.swift
@@ -11,12 +11,15 @@ import SimplyCoreAudio
 
 class OutputDeviceService {
     
+    private let throttleQueue = OperationQueue()
+    
     private var outputDevices: [OutputDevice] = []
     
     private var volumeChangedListener: ((OutputDevice, Float32, Float32)->(Bool))?
     private var deviceListChangedListener: (()->())?
     
     private var deviceListChangedObserver : NSObjectProtocol?
+    private var volumeChangedObserver : NSObjectProtocol? = nil
     
     private static func getOutputDevices(simplyCA : SimplyCoreAudio) -> [OutputDevice]{
         return simplyCA.allOutputDevices
@@ -44,10 +47,19 @@ class OutputDeviceService {
 
             self?.deviceListChangedListener?()
         }
+        
+        self.volumeChangedObserver = NotificationCenter.default.addObserver(forName: .deviceVolumeDidChange,
+                                                                            object: nil,
+                                                                            queue: .main) { (notification) in
+            self.handleVolumeChangedEvent()
+        }
     }
     
     deinit {
         if let observer = self.deviceListChangedObserver {
+            NotificationCenter.default.removeObserver(observer)
+        }
+        if let observer = volumeChangedObserver {
             NotificationCenter.default.removeObserver(observer)
         }
     }
@@ -56,6 +68,17 @@ class OutputDeviceService {
         self.volumeChangedListener = listener
         self.setVolumeChangedListenerToAllDevices()
       
+    }
+    
+    private func handleVolumeChangedEvent() {
+        let throttleOperation = VolumeChangedThrottleOperation() {
+            self.outputDevices.forEach { device in
+                device.syncVolumeWithSystem()
+            }
+        }
+        
+        self.throttleQueue.cancelAllOperations()
+        self.throttleQueue.addOperation(throttleOperation)
     }
     
     private func setVolumeChangedListenerToAllDevices(){
@@ -68,5 +91,29 @@ class OutputDeviceService {
     
     func setDeviceListChangedListener(listener: (()->())?){
         self.deviceListChangedListener = listener
+    }
+}
+
+private class VolumeChangedThrottleOperation: Operation {
+    
+    private static let throttleDelayMs = 96
+    private let onComplete : (() -> Void)!
+    
+    init(onComplete : @escaping () -> Void) {
+        self.onComplete = onComplete
+    }
+    
+    override func main() {
+//        NSLog("Trottling...")
+        sleepMs(durationMs: VolumeChangedThrottleOperation.throttleDelayMs)
+        
+        if (isCancelled){
+            return
+        } else {
+            DispatchQueue.main.async(){
+//                NSLog("Notifing...")
+                self.onComplete()
+            }
+        }
     }
 }
